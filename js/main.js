26dac4fb78c1834de8e4c6b7daf0763bae9cb0a5
DaiC <daira_02@hotmail.com>
2023-07-28 22:47:05 -0300

    arreglos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
- 
- 
- 
+ // Se declara un arreglo 'carrito' para almacenar los platillos seleccionados
  let carrito = [];
+ // Se seleccionan elementos del DOM mediante clases y etiquetas
  const menu = document.querySelector('.hamburguesa');
  const navegacion = document.querySelector('.navegacion');
  const imagenes = document.querySelectorAll('img');
@@ -11,21 +10,23 @@
  const btnPizza = document.querySelector('.pizza');
  const btnPostres = document.querySelector('.postres');
  const contenedorPlatillos = document.querySelector('.platillos');
+ // Se llaman a las funciones 'eventos()' y 'platillos()'.
  document.addEventListener('DOMContentLoaded',()=>{
      eventos();
      platillos();
  });
- 
+ // Se agrega un evento click al botón de menú para abrir la navegación
  const eventos = () =>{
      menu.addEventListener('click',abrirMenu);
  }
- 
+ // Función que muestra la navegación al hacer clic en el menú
  const abrirMenu = () =>{
       navegacion.classList.remove('ocultar');
       botonCerrar();
  }
- 
+ // Función que agrega un botón de cerrar en la navegación y lo asocia a la acción de cerrar el menú
  const botonCerrar = () =>{
+  // Creación de elementos del DOM
      const btnCerrar = document.createElement('p');
      const overlay  = document.createElement('div');
      overlay.classList.add('pantalla-completa');
@@ -38,7 +39,7 @@
      cerrarMenu(btnCerrar,overlay);
      
  }
- 
+ // Creación de un observador de intersecciones para cargar imágenes cuando son visibles en la pantalla
  const observer = new IntersectionObserver((entries, observer)=>{
          entries.forEach(entry=>{
              if(entry.isIntersecting){
@@ -49,12 +50,11 @@
          }); 
  });
  
- 
+ // Se observan las imágenes para cargarlas cuando sean visibles
  imagenes.forEach(imagen=>{
-    
      observer.observe(imagen);
  });
- 
+ // Función que cierra el menú al hacer clic en el botón de cerrar o en el overlay
  const cerrarMenu = (boton, overlay) =>{
      boton.addEventListener('click',()=>{
          navegacion.classList.add('ocultar');
@@ -68,7 +68,7 @@
          boton.remove();
      }
  }
- 
+ // Función que filtra los platillos por tipo y luego llama a 'mostrarPlatillos()'
  const platillos = () =>{
      let platillosArreglo = [];
      const platillos = document.querySelectorAll('.platillo');
@@ -83,7 +83,7 @@
      mostrarPlatillos(ensaladas, pastas, pizzas, postres, platillosArreglo);
  
  }
- 
+ // Función que muestra los platillos según el tipo seleccionado en el menú
  const mostrarPlatillos = (ensaladas, pastas, pizzas, postres, todos) =>{
      btnEnsaladas.addEventListener('click', ()=>{
          limpiarHtml(contenedorPlatillos);
@@ -108,26 +108,27 @@
          todos.forEach(todo=> contenedorPlatillos.appendChild(todo));
      });
  }
- 
+ // Función que limpia el contenido del contenedor
  const limpiarHtml = (contenedor) =>{
      while(contenedor.firstChild){
          contenedor.removeChild(contenedor.firstChild);
      }
  }
+ // Función que agrega un platillo al carrito y actualiza la lista del carrito
  function agregarAlCarrito(nombre, precio) {
     carrito.push({ nombre, precio });
     actualizarCarrito();
     console.log('Platillo agregado al carrito:', { nombre, precio });
   }
 
-
+// Función que vacía el carrito y actualiza la lista del carrito
   function vaciarCarrito() {
     carrito = [];
     actualizarCarrito();
     console.log('Carrito vaciado');
   }
 
-
+// Función que actualiza la lista del carrito y el total
   function actualizarCarrito() {
     const listaProductos = document.getElementById('lista-productos');
     const totalSpan = document.getElementById('total');
@@ -145,7 +146,7 @@
     console.log('Contenido del carrito:', carrito);
   }
 
- 
+ // Asignación de eventos click a los botones de "Agregar al carrito"
   const botonesAgregar = document.querySelectorAll('.btn-agregar-carrito');
 
   botonesAgregar.forEach((boton) => {
@@ -155,12 +156,13 @@
       agregarAlCarrito(nombre, precio);
     });
   });
-
+  // Asignación del evento click al botón de vaciar carrito
   const botonVaciar = document.getElementById('vaciar-carrito');
 
   botonVaciar.addEventListener('click', () => {
     vaciarCarrito();
   });
+  // Función que muestra un mensaje de error en el carrito
   function mostrarError(mensaje) {
     const mensajeError = document.createElement('p');
     mensajeError.textContent = mensaje;
@@ -169,7 +171,7 @@
     const contenedorCarrito = document.getElementById('carrito');
     contenedorCarrito.insertBefore(mensajeError, botonFinalizarCompra);
   }
-
+//Funcion que muestra un mensaje de finalizar compra
   const botonFinalizarCompra = document.getElementById('finalizar-compra');
 
 
@@ -180,4 +182,167 @@
       alert('¡Gracias por tu compra! Tu pedido ha sido procesado exitosamente.');
       vaciarCarrito();
     }
-  });
+  })
+  class Calendar {
+    constructor(id) {
+        this.cells = [];
+        this.selectedDate = null;
+        this.currentMonth = moment();
+        this.elCalendar = document.getElementById(id);
+        this.showTemplate();
+        this.elGridBody = this.elCalendar.querySelector('.grid__body');
+        this.elMonthName = this.elCalendar.querySelector('.month-name');
+        this.showCells();
+    }
+
+    showTemplate() {
+        this.elCalendar.innerHTML = this.getTemplate();
+        this.addEventListenerToControls();
+    }
+
+    getTemplate() {
+        let template = `
+            <div class="calendar__header">
+                <button type="button" class="control control--prev">&lt;</button>
+                <span class="month-name">dic 2019</span>
+                <button type="button" class="control control--next">&gt;</button>
+            </div>
+            <div class="calendar__body">
+                <div class="grid">
+                    <div class="grid__header">
+                        <span class="grid__cell grid__cell--gh">Lun</span>
+                        <span class="grid__cell grid__cell--gh">Mar</span>
+                        <span class="grid__cell grid__cell--gh">Mié</span>
+                        <span class="grid__cell grid__cell--gh">Jue</span>
+                        <span class="grid__cell grid__cell--gh">Vie</span>
+                        <span class="grid__cell grid__cell--gh">Sáb</span>
+                        <span class="grid__cell grid__cell--gh">Dom</span>
+                    </div>
+                    <div class="grid__body">
+
+                    </div>
+                </div>
+            </div>
+        `;
+        return template;
+    }
+
+    addEventListenerToControls() {
+        let elControls = this.elCalendar.querySelectorAll('.control');
+        elControls.forEach(elControl => {
+            elControl.addEventListener('click', e => {
+                let elTarget = e.target;
+                if (elTarget.classList.contains('control--next')) {
+                    this.changeMonth(true);
+                } else {
+                    this.changeMonth(false);
+                }
+                this.showCells();
+            });
+        });
+    }
+
+    changeMonth(next = true) {
+        if (next) {
+            this.currentMonth.add(1, 'months');
+        } else {
+            this.currentMonth.subtract(1, 'months');
+        }
+    }
+
+    showCells() {
+        this.cells = this.generateDates(this.currentMonth);
+        if (this.cells === null) {
+            console.error('No fue posible generar las fechas del calendario.');
+            return;
+        }
+
+        this.elGridBody.innerHTML = '';
+        let templateCells = '';
+        let disabledClass = '';
+        for (let i = 0; i < this.cells.length; i++) {
+            disabledClass = '';
+            if (!this.cells[i].isInCurrentMonth) {
+                disabledClass = 'grid__cell--disabled';
+            }
+            // <span class="grid__cell grid__cell--gd grid__cell--selected">1</span>
+            templateCells += `
+                <span class="grid__cell grid__cell--gd ${disabledClass}" data-cell-id="${i}">
+                    ${this.cells[i].date.date()}
+                </span>
+            `;
+        }
+        this.elMonthName.innerHTML = this.currentMonth.format('MMM YYYY');
+        this.elGridBody.innerHTML = templateCells;
+        this.addEventListenerToCells();
+    }
+
+    generateDates(monthToShow = moment()) {
+        if (!moment.isMoment(monthToShow)) {
+            return null;
+        }
+        let dateStart = moment(monthToShow).startOf('month');
+        let dateEnd = moment(monthToShow).endOf('month');
+        let cells = [];
+
+        // Encontrar la primer fecha que se va a mostrar en el calendario
+        while (dateStart.day() !== 1) {
+            dateStart.subtract(1, 'days');
+        }
+
+        // Encontrar la última fecha que se va a mostrar en el calendario
+        while (dateEnd.day() !== 0) {
+            dateEnd.add(1, 'days');
+        }
+
+        // Genera las fechas del grid
+        do {
+            cells.push({
+                date: moment(dateStart),
+                isInCurrentMonth: dateStart.month() === monthToShow.month()
+            });
+            dateStart.add(1, 'days');
+        } while (dateStart.isSameOrBefore(dateEnd));
+
+        return cells;
+    }
+
+    addEventListenerToCells() {
+        let elCells = this.elCalendar.querySelectorAll('.grid__cell--gd');
+        elCells.forEach(elCell => {
+            elCell.addEventListener('click', e => {
+                let elTarget = e.target;
+                if (elTarget.classList.contains('grid__cell--disabled') || elTarget.classList.contains('grid__cell--selected')) {
+                    return;
+                }
+                // Deselecionar la celda anterior
+                let selectedCell = this.elGridBody.querySelector('.grid__cell--selected');
+                if (selectedCell) {
+                    selectedCell.classList.remove('grid__cell--selected');
+                }
+                // Selecionar la nueva celda
+                elTarget.classList.add('grid__cell--selected');
+                this.selectedDate = this.cells[parseInt(elTarget.dataset.cellId)].date;
+                // Lanzar evento change
+                this.elCalendar.dispatchEvent(new Event('change'));
+            });
+        });
+    }
+
+    getElement() {
+        return this.elCalendar;
+    }
+
+    value() {
+        return this.selectedDate;
+    }
+}
+let calendar = new Calendar('calendar');
+        calendar.getElement().addEventListener('change', e => {
+            console.log(calendar.value().format('LLL'));
+        });
+
+        let calendar2 = new Calendar('calendar2');
+        calendar2.getElement().addEventListener('change', e => {
+            console.log(calendar2.value().format('LLL'));
+        });
